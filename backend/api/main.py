5e0b89dcb33a89530defa8083173b43f2cc52888
zansi <zansi@fastapi.com>
2022-08-29 23:55:44 +0530

    add product detail

--- backend/api/main.py
+++ backend/api/main.py
@@ -7,19 +7,13 @@ from backend.dependencies.add_data import AddData
 from backend.dependencies.delete_data import DeleteData
 from backend.dependencies.get_data import GetData
 from backend.dependencies.login import LoginUser
-from backend.dependencies.registration import NewUser, TBRegister
+from backend.dependencies.registration import NewUser
 from backend.dependencies.update_data import UpdateData
 from backend.models.database.product_detail import TBProductDetail
-from backend.models.database.product_electronic import TBDelivery
-from backend.models.database.status import TBStatus
-from backend.models.database.tb_brand import TBBrand
-from backend.models.database.tb_category import TBCategory
-from backend.models.database.tb_register import TBRegister
 from backend.schemas.brand import BrandDetail
 from backend.schemas.category import CategoryDetail
 from backend.schemas.delete_model.delete_cart import DeleteCart
 from backend.schemas.h_accessories import Producti
-from backend.schemas.p_electronic import Delivery, Status
 from backend.schemas.place_order import PlaceOrder
 from backend.schemas.product import Product
 from backend.schemas.product_details import ProductDetail
@@ -28,121 +22,98 @@ from backend.schemas.updateModel.update_cart import UpdateQuantity
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
-       title="e-Shopping",
-       description="This is online shopping site for customer."
+    title="e-Shopping",
+    description="This is online shopping site for customer."
 )
 
-Base.metadata.create_all(bind=connection)
+Base.metadata.create_all(bind = connection)
 app.add_middleware(
     CORSMiddleware,
-    allow_credentials=True,
+    allow_credentials = True,
     allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 
-@app.post("/auth/registeration",tags=['auth'])
+@app.post("/auth/registeration", tags = ['auth'])
 def create_user(user: CreateUser, db: Session = Depends(start_session), register_detail: NewUser = Depends(NewUser)):
+    
     return register_detail.create_user(user, db)
 
 
-@app.post("/auth/login",tags=['auth'])
+@app.post("/auth/login", tags = ['auth'])
 def login(form_data: Login, db: Session = Depends(start_session), login_detail: LoginUser = Depends(LoginUser)):
+    
     return login_detail.login_detail(form_data, db)
 
 
-@app.post('/brand', tags=['brand'])
+# Add Brand
+@app.post('/brand', tags = ['brand'])
 def add_brand(brand: BrandDetail, db: Session = Depends(start_session), brand_detail: NewUser = Depends(NewUser)):
+    
     return brand_detail.add_brand(brand, db)
 
-
-@app.post('/category', tags=['category'])
+# Add Category
+@app.post('/category', tags = ['category'])
 def add_category(category: CategoryDetail, category_detail: AddData = Depends(AddData)):
+    
     return category_detail.add_category(category)
 
 
-@app.post('/product', tags=['product'])
+# Add Product
+@app.post('/product', tags = ['product'])
 def add_product(product: Product, product_detail: AddData = Depends(AddData)):
 
     return product_detail.add_product(product)
 
 
-@app.post('/product/product1', tags=['product'])
-def add(product: Producti, db: Session = Depends(start_session)):
-    for i, k in product.dict_type.items():
+# Add product details
+@app.post('/product/details', tags = ['product'])
+def add_product_details(product: Producti, detail: AddData = Depends(AddData)):
 
-        if type(k) == list:
-            for y in k:
-                product1 = TBProductDetail(
-                    detail_attribute=i, detail_value=y, product_id=product.product_id)
-                db.add(product1)
-                db.commit()
-                db.refresh(product1)
-        else:
-            product1 = TBProductDetail(
-                detail_attribute=i, detail_value=k, product_id=product.product_id)
-            db.add(product1)
-            db.commit()
-            db.refresh(product1)
-    return product
+   return detail.add_detail(product)
 
 
+# search for category , product and brand
 @app.get('/product')
 def search_product(search_for: str = '', name: Optional[str] = '', id: Optional[int] = None, get_product_detail: GetData = Depends(GetData)):
     return get_product_detail.get_data(search_for, name, id)
 
 
-@app.get('/product/get_brand', tags=['product'])
-def get_brand(db: Session = Depends(start_session)):
+@app.post('/product/images', tags = ['product'])
+def add_produck_images(file1: list[UploadFile] = File(...), product_id: int = Form(...), color: str = Form(...), total_product: int = Form(...), add_images: AddData = Depends(AddData)):
 
-    brand = db.query(TBBrand).all()
-    category = db.query(TBCategory).all()
-
-    return brand, category
+    return add_images.add_images(file1, product_id, color, total_product, db)
 
 
-@app.post('/product/add_product_images', tags=['product'])
-def add_produck_stock(file1: list[UploadFile] = File(...), product_id: int = Form(...), color: str = Form(...), total_product: int = Form(...), add_images: AddData = Depends(AddData)):
-    return add_images.add_produck_images(file1, product_id, color, total_product, db)
-
-
-@app.post('/product/add_product_details', tags=['product'])
+# add product details
+@app.post('/product/details', tags = ['product'])
 def add_product_detail(product: ProductDetail, db: Session = Depends(start_session), product_detail: AddData = Depends(AddData)):
 
     return product_detail.add_product_detail(product, db)
 
 
-@app.post('/cart/add_cart', tags=['cart'])
-def add_cart(file1: UploadFile = File(...), product_name: str = Form(...), price: int = Form(...), quantity: int = Form(...), r_id: int = Form(...), cart_detail: AddData = Depends(AddData)):
+# add in to cart
+@app.post('/cart', tags = ['cart'])
+def add_in_cart(file1: UploadFile = File(...), product_name: str = Form(...), price: int = Form(...), quantity: int = Form(...), r_id: int = Form(...), cart_detail: AddData = Depends(AddData)):
 
-    return cart_detail.add_cart(file1, product_name, price, quantity, r_id)
+    return cart_detail.add_into_cart(file1, product_name, price, quantity, r_id)
 
 
-@app.put('/cart/update_quantity', tags=['cart'])
+# update quantity
+@app.put('/cart/quantity', tags = ['cart'])
 def update_cart_quantity(details: UpdateQuantity, db: Session = Depends(start_session), update_cart: UpdateData = Depends(UpdateData)):
     return update_cart.update_cart(details, db)
 
 
-@app.delete('/cart/delete', tags=['cart'])
+# delete cart user
+@app.delete('/cart', tags = ['cart'])
 def delete_cart(cart: DeleteCart, db: Session = Depends(start_session), delete_cart_detail: DeleteData = Depends(DeleteData)):
     return delete_cart_detail.delete_cart(cart, db)
 
 
-@app.post('/place_order/in_place_order', tags=['place_order'])
-def in_place_order(place_order: PlaceOrder, place_order_detail: AddData = Depends(AddData)):
+# add in to place order
+@app.post('/place_order', tags = ['place_order'])
+def add_in_placeorder(place_order: PlaceOrder, place_order_detail: AddData = Depends(AddData)):
     return place_order_detail.add_in_place_order(place_order)
-
-
-@app.post('/delivery')
-def file_upload(status:Delivery,db: Session = Depends(start_session)):
-    statu = TBDelivery(delivery_type = status.delivery_type, status_id = status.status_id)
-    db.add(statu)
-    db.commit()
-    db.refresh(statu)
-    return statu
-
-@app.get('/delivery')
-def get_delivery(db: Session = Depends(start_session)):
-    defd = db.query(TBStatus,TBDelivery).join(TBDelivery,TBStatus.status_id == TBDelivery.status_id).all()
-    return defd
--- backend/dependencies/add_data.py
+++ backend/dependencies/add_data.py
@@ -10,16 +10,18 @@ from backend.models.database.tb_category import TBCategory
 from backend.database.session import start_session
 from requests import Session
 from fastapi import Depends, UploadFile, File, Form
+from backend.schemas.h_accessories import Producti
 from backend.schemas.place_order import PlaceOrder
 import random
 from backend.schemas.product import Product
-from backend.schemas.product_details import ProductDetail, ProductImage
+from backend.schemas.product_details import ProductImage
+
 
 class AddData(AddDataInterface):
-    def __init__(self,db: Session = Depends(start_session)):
-          self.db = db
-       
-    def _add_in_database(self,add_new_data):
+    def __init__(self, db: Session = Depends(start_session)):
+        self.db = db
+
+    def _add_in_database(self, add_new_data):
         self.db.add(add_new_data)
         self.db.commit()
         self.db.refresh(add_new_data)
@@ -39,85 +41,113 @@ class AddData(AddDataInterface):
         return image_name
 
     def add_category(self, category: CategoryDetail):
-        new_category = TBCategory(category_name = category.category_name, brand_name = category.brand_name,
-                                  b_id=category.b_id)
-        return AddData._add_in_database(self,new_category)
+        new_category = TBCategory(category_name = category.category_name, 
+            brand_name = category.brand_name,
+            b_id = category.b_id)
+
+        return AddData._add_in_database(self, new_category)
 
     def add_product(self, product: Product):
-        new_product = TBProduct(name=product.name,
-                                description=product.description,
-                                price=product.price,
-                                in_the_box=product.in_the_box,
-                                model_name=product.model_name,
-                                brand_name=product.brand_name,
-                                category_name=product.category_name,
-                                is_electronic=product.is_electronic,
-                                c_id=product.c_id)
+        new_product = TBProduct(name = product.name,
+                                description = product.description,
+                                price = product.price,
+                                in_the_box = product.in_the_box,
+                                model_name = product.model_name,
+                                brand_name = product.brand_name,
+                                category_name = product.category_name,
+                                is_electronic = product.is_electronic,
+                                c_id = product.c_id)
 
         product = AddData._add_in_database(new_product)
 
         return product
 
-    def add_produck_images(self, images: list[UploadFile] = File(...), product_id: int = Form(...), color: str = Form(...), total_stock: int = Form(...),  db: Session = Depends(start_session)):
-        add = []
-        for image in images:
+    def add_images(self, images_list: list[UploadFile] = File(...), product_id: int = Form(...), color: str = Form(...), total_stock: int = Form(...),  db: Session = Depends(start_session)):
+        image_list = []
+        for image in images_list:
 
             image_name = AddData.__rename_image_name(image, "product")
 
-            data = ProductImage(image=image_name,
-                                color=color,
-                                total_stock=total_stock,
-                                product_id=product_id
+            data = ProductImage(image = image_name,
+                                color = color,
+                                total_stock = total_stock,
+                                product_id = product_id
                                 )
 
-            new_images = TBProductStock(
-                product_id=data.product_id, 
-                image_name=data.image,
-                color=data.color, 
-                total_stock=data.total_stock)
-                
-            AddData._add_in_database(new_images, db)
-            add.append(new_images)
+            images = TBProductStock(
+                product_id = data.product_id,
+                image_name = data.image,
+                color = data.color,
+                total_stock = data.total_stock)
+
+            AddData._add_in_database(images, db)
+            image_list.append(images)
             print(data)
 
-        return add
+        return image_list
 
-    def add_product_detail(self, product_detail: ProductDetail):
-        detail_list = []
-        detail_dict = {}
-        for t, v in zip(product_detail.detail_attribute_name, product_detail.detail_attribute_value):
-            evn = []
-            for i in v:
+    # def add_product_detail(self, product_detail: ProductDetail):
+    #     detail_list = []
+    #     detail_dict = {}
+    #     for t, v in zip(product_detail.detail_attribute_name, product_detail.detail_attribute_value):
+    #         evn = []
+    #         for i in v:
 
-                product = TBProductDetail(
-                    detail_attribute=t, detail_value=i, product_id=product_detail.product_id)
-                AddData._add_in_database(self,product)
-                evn.append(i)
-                if t in detail_dict:
-                    detail_dict[t] = evn
+    #             product = TBProductDetail(
+    #                 detail_attribute=t, detail_value=i, product_id=product_detail.product_id)
+    #             AddData._add_in_database(self,product)
+    #             evn.append(i)
+    #             if t in detail_dict:
+    #                 detail_dict[t] = evn
 
-                else:
-                    detail_dict[t] = i
-        detail_list.append(detail_dict)
+    #             else:
+    #                 detail_dict[t] = i
+    #     detail_list.append(detail_dict)
 
-        return detail_list
+    #     return detail_list
+    def add_detail(self, product: Producti):
+        for i, k in product.dict_type.items():
 
-    def add_cart(self, file1: UploadFile, product_name: str = Form(...), price: int = Form(...), quantity: int = Form(...), r_id: int = Form(...)):
+            if type(k) == list:
+                for y in k:
+                    detail = TBProductDetail(
+                        detail_attribute=i,
+                        detail_value=y,
+                        product_id=product.product_id)
+                    AddData._add_in_database(self, detail)
+
+            else:
+                detail = TBProductDetail(
+                    detail_attribute=i,
+                    detail_value=k,
+                    product_id=product.product_id)
+                AddData._add_in_database(self, detail)
+
+        return product
+
+    def add_into_cart(self, file1: UploadFile, product_name: str = Form(...), price: int = Form(...), quantity: int = Form(...), r_id: int = Form(...)):
         mapping_cart = CartDetail(
             product_name=product_name, product_price=price, quantity=quantity, r_id=r_id)
+
         total_price = quantity * price
 
         image_name = AddData.__rename_image_name(file1, "cart")
         cart = TBCart(product_name=mapping_cart.product_name,
                       product_price=mapping_cart.product_price,
                       quantity=mapping_cart.quantity,
-                      total_price=total_price, c_image=image_name,
+                      total_price=total_price,
+                      c_image=image_name,
                       r_id=mapping_cart.r_id)
 
-        return AddData._add_in_database(self,cart)
+        return AddData._add_in_database(self, cart)
 
     def add_in_place_order(self, in_place_order: PlaceOrder):
-        place_order = TBPlaceOrder(username=in_place_order.username, email=in_place_order.email, products=in_place_order.products,
-                                   total_price=in_place_order.total_price,  address=in_place_order.address, delivery_type=in_place_order.delivery_type, r_id=in_place_order.r_id)
+        place_order = TBPlaceOrder(username=in_place_order.username,
+                                   email=in_place_order.email,
+                                   products=in_place_order.products,
+                                   total_price=in_place_order.total_price,
+                                   address=in_place_order.address,
+                                   delivery_type=in_place_order.delivery_type,
+                                   r_id=in_place_order.r_id)
 
-        return AddData._add_in_database(self,place_order)
+        return AddData._add_in_database(self, place_order)
